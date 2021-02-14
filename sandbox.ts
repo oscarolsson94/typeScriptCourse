372921f0b66d52525f7e4340ca1ae35e4f8dc197
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-14 14:16:02 +0100

    type checks before compiling

--- sandbox.ts
+++ sandbox.ts
@@ -1,13 +1,39 @@
-const character = 'mario';
+// const character = 'superman';
 
-console.log(character);
+// console.log(character);
 
-const inputs = document.querySelectorAll('input'); //grabs all input tags
+// const inputs = document.querySelectorAll('input'); //grabs all input tags
 
-console.log(inputs);
+// console.log(inputs); //nodelist
 
 // to compile this to javaScript:
 // tsc sandbox.ts sandbox.js      <------ in terminal
 // tsc sandbox.ts      <----- if both files have the same name, this is enough
 // if the javaScript-file doesn't exist, it will be created upon compiling
 
+// inputs.forEach(input => {
+//     console.log(input);
+// });
+
+// for automatic compilation on save:
+// tsc sandbox.ts -w         <----------- terminal
+
+// Type Basics:
+// let character = 'mario';
+// let age = 30;
+// let isBlackBelt = false;
+
+//We can change the variable value, bot NOT the type of the variable.
+
+// const circ = (diameter: number) => { //add type of the argument
+//     return diameter * Math.PI
+// };
+
+// console.log(circ('hello')); //does not make sense. Logs "NaN" to the console
+// console.log(circ(7.5)); // alows us to type check be fore compiling our code.
+
+// arrays:
+
+
+
+// objects:
