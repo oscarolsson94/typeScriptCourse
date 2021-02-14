e3e717b164226a0f8532b8c9140057d493445e81
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-14 13:58:19 +0100

    compiling into js

--- sandbox.ts
+++ sandbox.ts
@@ -1,7 +1,13 @@
-var character = 'mario';
+const character = 'mario';
+
 console.log(character);
-var inputs = document.querySelectorAll('input'); //grabs all input tags
+
+const inputs = document.querySelectorAll('input'); //grabs all input tags
+
 console.log(inputs);
+
 // to compile this to javaScript:
 // tsc sandbox.ts sandbox.js      <------ in terminal
 // tsc sandbox.ts      <----- if both files have the same name, this is enough
+// if the javaScript-file doesn't exist, it will be created upon compiling
+
