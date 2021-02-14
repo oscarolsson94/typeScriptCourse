2003a3eb4d2dd10040626f79ba3458b109062268
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-14 14:41:21 +0100

    explicit types in typeScript

--- sandbox.ts
+++ sandbox.ts
@@ -42,22 +42,49 @@
 // this array can contain strings, numbers and booleans
 
 // objects:
-let ninja = {
-    name: 'mario',
-    belt: 'black',
-    age: 30
-}
+// let ninja = {
+//     name: 'mario',
+//     belt: 'black',
+//     age: 30
+// }
 
-ninja.age = 40; // works fine
-ninja.name = 'ryu'; // fine
+// ninja.age = 40; // works fine
+// ninja.name = 'ryu'; // fine
 // ninja.age = '30'; // does NOT work, cannot change the type of age to string
 
 // ninja.skills = ['fighting', 'sneaking']; // can NOT add new properties
 
-ninja = { // can assign it to a new object, but the properties MUST match
+// ninja = { // can assign it to a new object, but the properties MUST match
 
-    name: 'yoshi',
-    belt: 'orange',
-    age: 40
+//     name: 'yoshi',
+//     belt: 'orange',
+//     age: 40
 
-}
+// }
+
+// explicit types
+// let character: string; // initialize a variable but dont assign it a value
+// let age: number;
+// let isLoggedIn: boolean;
+
+// arrays
+// let ninjas: string[] = []; // can only be an array of strings. Initialized as an empty array.
+
+// union types
+// let mixed: (string|number)[] = []; // can now store strings and numbers in the array
+
+// let uid: string | number; // uid can now be a string or a number
+
+// objects
+// let ninjaOne: object; // it can now be any type of object. Ex and array. Can also have any properties.
+// ninjaOne = { name: 'yoshi', age: 30 };
+
+// let ninjaTwo: { // more specific, must be an object with ALL of the following properties
+//     name: string,
+//     age: number,
+//     beltColour:string
+// }
+
+// ninjaTwo = {name:'mario', age:20, beltColour:'black', skills:[]} // does NOT work, can't add "skills"-property.
+
+//
