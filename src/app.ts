f4702b905155cb8d8846c36b961f65fe4b938712
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-16 08:45:31 +0100

    type casting and DOM work

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,22 @@
 // const form = document.querySelector('form')!; // typescript knows what kind of element it is, and what methods it has available
 
 // const form = document.querySelector('.new-item-form'); //typescript doesnt know what kind of element it is, only that it has a class
-const form = document.querySelector('.new-item-form') as HTMLFormElement;  //type cast it into a Form-element
+// const form = document.querySelector('.new-item-form') as HTMLFormElement;  //type cast it into a Form-element
 
+// // inputs --------------------------
+// const type = document.querySelector('#type')! as HTMLSelectElement;
+// const tofrom = document.querySelector('#tofrom')! as HTMLInputElement;
+// const details = document.querySelector('#details')! as HTMLInputElement;
+// const amount = document.querySelector('#amount')! as HTMLInputElement;
 
+// form.addEventListener('submit', (e: Event) => {
+//     e.preventDefault(); // prevents browser from refreshing on submit
+
+//     console.log( // grab values from the input fields (as strings)
+//         type.value,
+//         tofrom.value,
+//         details.value,
+//         amount.valueAsNumber // grab the value as a number instead of a string
+//     )
+
+// })
