1b53adab0a9cbeffb84bcbe15ef8c1ee7fef62e6
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-16 08:36:19 +0100

    dom work in tsc

--- public/app.js
+++ public/app.js
@@ -1,1 +1,6 @@
 "use strict";
+// const anchor = document.querySelector('a')!; //<---- add ! if you are sure there is an <a>-tag in the DOM
+// console.log(anchor.href) // typeScript doesnt know if there is an <a>-tag in the dom or not
+// const form = document.querySelector('form')!; // typescript knows what kind of element it is, and what methods it has available
+// const form = document.querySelector('.new-item-form'); //typescript doesnt know what kind of element it is, only that it has a class
+var form = document.querySelector('.new-item-form'); //type cast it into a Form-element
